63f0a95121209610203dcad1e18a9bf64c0ab15f
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2017-10-23 09:35:56 +1000

    Apply string literal formatting in display mode (#25)

---
+++ test/Serilog.Sinks.Console.Tests/Formatting/ThemedDisplayValueFormatterTests.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Formatting;
+using Serilog.Sinks.SystemConsole.Themes;
+using Xunit;
+
+namespace Serilog.Sinks.Console.Tests.Formatting
+{
+    public class ThemedDisplayValueFormatterTests
+    {
+        [Theory]
+        [InlineData("Hello", null, "\"Hello\"")]
+        [InlineData("Hello", "l", "Hello")]
+        public void StringFormattingIsApplied(string s, string format, string expected)
+        {
+            var f = new ThemedDisplayValueFormatter(ConsoleTheme.None, null);
+            var sw = new StringWriter();
+            f.FormatLiteralValue(new ScalarValue(s), sw, format);
+            var actual = sw.ToString();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
