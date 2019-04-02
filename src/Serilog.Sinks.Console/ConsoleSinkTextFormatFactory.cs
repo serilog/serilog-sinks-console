a860411a561a0a7b0c230ff3992ee94fcfce3f77
Duncan Roosma <duncan@omniaretail.com>
2019-04-02 19:17:57 +0200

    [#56] adds ITextFormatter factory to construct OutputTemplateRenderer

---
+++ src/Serilog.Sinks.Console/ConsoleSinkTextFormatFactory.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+using Serilog.Formatting;
+using Serilog.Sinks.SystemConsole.Output;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace Serilog
+{
+    /// <summary>
+    /// Factory utility to construct OutputTemplateRenderer ITextFormatter
+    /// </summary>
+    public class ConsoleSinkTextFormatFactory
+    {
+        private const string defaultConsoleOutputTemplate = "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}";
+        private IFormatProvider _formatProvider;
+        private string _outputTemplate;
+        private ConsoleTheme _theme;
+
+        private ConsoleSinkTextFormatFactory()
+        {
+        }
+
+        /// <summary>
+        /// Creates new instance of ConsoleSinkTextFormatFactory
+        /// </summary>
+        /// <returns></returns>
+        public static ConsoleSinkTextFormatFactory Create()
+            => new ConsoleSinkTextFormatFactory();
+
+        /// <summary>
+        /// Sets format provider
+        /// </summary>
+        /// <param name="formatProvider"></param>
+        /// <returns></returns>
+        public ConsoleSinkTextFormatFactory With(IFormatProvider formatProvider)
+        {
+            _formatProvider = formatProvider;
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the output template format
+        /// </summary>
+        /// <param name="outputTemplate"></param>
+        /// <returns></returns>
+        public ConsoleSinkTextFormatFactory WithOutPutTemplate(string outputTemplate)
+        {
+            _outputTemplate = outputTemplate;
+            return this;
+        }
+
+        /// <summary>
+        /// Sets console theme
+        /// </summary>
+        /// <param name="theme"></param>
+        /// <returns></returns>
+        public ConsoleSinkTextFormatFactory With(ConsoleTheme theme)
+        {
+            _theme = theme;
+            return this;
+        }
+
+        /// <summary>
+        /// Builds an instance of ITextFormatter from OutputTemplateRenderer
+        /// </summary>
+        /// <returns>OutputTemplateRenderer</returns>
+        public ITextFormatter Build()
+        {
+            var outputTemplate = string.IsNullOrWhiteSpace(_outputTemplate) ? defaultConsoleOutputTemplate : _outputTemplate;
+            var appliedTheme = ConsoleThemeSelector.AppliedTheme(_theme);
+
+            return new OutputTemplateRenderer(appliedTheme, outputTemplate, _formatProvider);
+        }
+    }
+}
