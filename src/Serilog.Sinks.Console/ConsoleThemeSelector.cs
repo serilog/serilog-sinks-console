17802702ff22b3d12aab8a9055b6a3d16ac359d6
Duncan Roosma <duncan@omniaretail.com>
2019-04-02 19:15:24 +0200

    [#56] add ConsoleThemeSelector to centralize ConsoleTheme decision

---
+++ src/Serilog.Sinks.Console/ConsoleThemeSelector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace Serilog
+{
+    internal static class ConsoleThemeSelector
+    {
+        public static ConsoleTheme AppliedTheme(ConsoleTheme theme)
+            => Console.IsOutputRedirected || Console.IsErrorRedirected
+                ? ConsoleTheme.None
+                : theme ?? SystemConsoleThemes.Literate;
+    }
+}
