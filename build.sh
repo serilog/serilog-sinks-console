c521c7abcafcc7508bb0a37d096f6224a3f18858
Simon Cropp <simon.cropp@gmail.com>
2019-08-22 12:13:51 +1000

    simplify build

--- build.sh
+++ build.sh
@@ -5,19 +5,14 @@ dotnet --info
 dotnet restore
 
 for path in src/**/*.csproj; do
-    dotnet build -f netstandard1.3 -c Release ${path}
     dotnet build -f netstandard2.0 -c Release ${path}
-    dotnet build -f netcoreapp1.1 -c Release ${path}
 done
 
 for path in test/*.Tests/*.csproj; do
-    dotnet test -f netcoreapp1.1 -c Release ${path}
     dotnet test -f netcoreapp2.2 -c Release ${path}
 done
 
 for path in sample/ConsoleDemo/*.csproj; do
-    dotnet build -f netcoreapp1.1 -c Release ${path}
-    dotnet run -f netcoreapp1.1 --project ${path}
     dotnet build -f netcoreapp2.2 -c Release ${path}
     dotnet run -f netcoreapp2.2 --project ${path}
 done
