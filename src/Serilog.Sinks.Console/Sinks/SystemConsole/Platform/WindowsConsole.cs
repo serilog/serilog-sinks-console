06f55485415001886725f0a27a03b54339ad1da9
Sergey Komisarchik <skomisarchik@usetech.ru>
2020-06-15 09:39:49 +0300

    disable quick edit mode

--- src/Serilog.Sinks.Console/Sinks/SystemConsole/Platform/WindowsConsole.cs
+++ src/Serilog.Sinks.Console/Sinks/SystemConsole/Platform/WindowsConsole.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2017 Serilog Contributors
+﻿// Copyright 2017-2020 Serilog Contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,16 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#if PINVOKE
 using System;
 using System.Runtime.InteropServices;
-#endif
 
 namespace Serilog.Sinks.SystemConsole.Platform
 {
     static class WindowsConsole
     {
-#if PINVOKE
         public static void EnableVirtualTerminalProcessing()
         {
 #if RUNTIME_INFORMATION
@@ -31,16 +28,25 @@ namespace Serilog.Sinks.SystemConsole.Platform
             if (Environment.OSVersion.Platform != PlatformID.Win32NT)
                 return;
 #endif
-            var stdout = GetStdHandle(StandardOutputHandleId);
-            if (stdout != (IntPtr)InvalidHandleValue && GetConsoleMode(stdout, out var mode))
+            var stdout = GetStdHandle(STD_OUTPUT_HANDLE);
+            if (stdout != (IntPtr)INVALID_HANDLE && GetConsoleMode(stdout, out var mode))
             {
-                SetConsoleMode(stdout, mode | EnableVirtualTerminalProcessingMode);
+                SetConsoleMode(stdout, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+            }
+
+            var stdin = GetStdHandle(STD_INPUT_HANDLE);
+            if (stdin != (IntPtr)INVALID_HANDLE && GetConsoleMode(stdin, out mode))
+            {
+                SetConsoleMode(stdin, mode & ~ENABLE_QUICK_EDIT_MODE);
             }
         }
 
-        const int StandardOutputHandleId = -11;
-        const uint EnableVirtualTerminalProcessingMode = 4;
-        const long InvalidHandleValue = -1;
+        // https://docs.microsoft.com/en-us/windows/console/setconsolemode
+        const int STD_OUTPUT_HANDLE = -11;
+        const int STD_INPUT_HANDLE = -10;
+        const uint ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004;
+        const uint ENABLE_QUICK_EDIT_MODE = 0x0040;
+        const long INVALID_HANDLE = -1;
 
         [DllImport("kernel32.dll", SetLastError = true)]
         static extern IntPtr GetStdHandle(int handleId);
@@ -50,10 +56,5 @@ namespace Serilog.Sinks.SystemConsole.Platform
 
         [DllImport("kernel32.dll", SetLastError = true)]
         static extern bool SetConsoleMode(IntPtr handle, uint mode);
-#else
-        public static void EnableVirtualTerminalProcessing()
-        {
-        }
-#endif
     }
 }
