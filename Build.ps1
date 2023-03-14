d76fe3b41860afd3506464ccee09ff2ed36e81cd
Ivan Maximov <sungam3r@yandex.ru>
2023-03-14 10:38:13 +0300

    fix CI 2

--- Build.ps1
+++ Build.ps1
@@ -1,16 +1,11 @@
 Write-Output "build: Build started"
 
-& dotnet --info
-& dotnet --list-sdks
-
 Push-Location $PSScriptRoot
 
 if(Test-Path .\artifacts) {
     Write-Output "build: Cleaning .\artifacts"
 	Remove-Item .\artifacts -Force -Recurse
 }
-
-& dotnet restore --no-cache
 
 $branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
 $revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
@@ -21,42 +16,20 @@ $buildSuffix = @{ $true = "$($suffix)-$($commitHash)"; $false = "$($branch)-$($c
 Write-Output "build: Package version suffix is $suffix"
 Write-Output "build: Build version suffix is $buildSuffix"
 
-foreach ($src in Get-ChildItem src/*) {
-    Push-Location $src
+& dotnet build --configuration Release --version-suffix=$buildSuffix /p:ContinuousIntegrationBuild=true
 
-    Write-Output "build: Packaging project in $src"
+if($LASTEXITCODE -ne 0) { exit 1 }
 
-    & dotnet build -c Release --version-suffix=$buildSuffix
-    if ($suffix) {
-        & dotnet pack -c Release --include-source -o ..\..\artifacts --version-suffix=$suffix --no-build
-    } else {
-        & dotnet pack -c Release --include-source -o ..\..\artifacts --no-build
-    }
-    if($LASTEXITCODE -ne 0) { exit 1 }
-
-    Pop-Location
+if($suffix) {
+    & dotnet pack src\Serilog.Sinks.Console --configuration Release --no-build --no-restore -o artifacts --version-suffix=$suffix
+} else {
+    & dotnet pack src\Serilog.Sinks.Console --configuration Release --no-build --no-restore -o artifacts
 }
 
-foreach ($sample in Get-ChildItem sample/*) {
-    Push-Location $sample
+if($LASTEXITCODE -ne 0) { exit 2 }
 
-    Write-Output "build: Testing project in $sample"
+Write-Output "build: Testing"
 
-    & dotnet build -c Release --version-suffix=$buildSuffix
-    if($LASTEXITCODE -ne 0) { exit 3 }
+& dotnet test test\Serilog.Sinks.Console.Tests --configuration Release --no-build --no-restore
 
-    Pop-Location
-}
-
-foreach ($test in Get-ChildItem test/*.Tests) {
-    Push-Location $test
-
-	Write-Output "build: Testing project in $test"
-
-    & dotnet test -c Release
-    if($LASTEXITCODE -ne 0) { exit 3 }
-
-    Pop-Location
-}
-
-Pop-Location
+if($LASTEXITCODE -ne 0) { exit 3 }
