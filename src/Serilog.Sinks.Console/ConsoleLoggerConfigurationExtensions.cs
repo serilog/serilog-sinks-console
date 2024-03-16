cf2c80d5f4839e1a77f70a9d58b3f34ba3baf03c
Ivan Maximov <sungam3r@yandex.ru>
2024-03-16 11:18:14 +0300

    Support NO_COLOR environment variable

--- src/Serilog.Sinks.Console/ConsoleLoggerConfigurationExtensions.cs
+++ src/Serilog.Sinks.Console/ConsoleLoggerConfigurationExtensions.cs
@@ -59,14 +59,15 @@ namespace Serilog
             IFormatProvider? formatProvider = null,
             LoggingLevelSwitch? levelSwitch = null,
             LogEventLevel? standardErrorFromLevel = null,
-            ConsoleTheme? theme = null, 
+            ConsoleTheme? theme = null,
             bool applyThemeToRedirectedOutput = false,
             object? syncRoot = null)
         {
             if (sinkConfiguration is null) throw new ArgumentNullException(nameof(sinkConfiguration));
             if (outputTemplate is null) throw new ArgumentNullException(nameof(outputTemplate));
 
-            var appliedTheme = !applyThemeToRedirectedOutput && (System.Console.IsOutputRedirected || System.Console.IsErrorRedirected) ?
+            // see https://no-color.org/
+            var appliedTheme = !applyThemeToRedirectedOutput && (System.Console.IsOutputRedirected || System.Console.IsErrorRedirected) || !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("NO_COLOR")) ?
                 ConsoleTheme.None :
                 theme ?? SystemConsoleThemes.Literate;
 
