5a730233d1963441c522c2ff196b2fee799dad33
Manuel Rinnhofer <manuel.rinnhofer@gmail.com>
2024-12-04 22:23:39 +0100

    Unit test setup streamlined with earlier existing similar test setup

--- test/Serilog.Sinks.Console.Tests/Output/OutputTemplateRendererTests.cs
+++ test/Serilog.Sinks.Console.Tests/Output/OutputTemplateRendererTests.cs
@@ -406,7 +406,8 @@ public class OutputTemplateRendererTests
     {
         var logTimestampWithTimeZoneOffset = DateTimeOffset.Parse("2024-09-03T14:15:16.079+02:00", CultureInfo.InvariantCulture);
         var formatter = new OutputTemplateRenderer(ConsoleTheme.None, "{Timestamp:yyyy-MM-dd HH:mm:ss}", CultureInfo.InvariantCulture);
-        var evt = new LogEvent(logTimestampWithTimeZoneOffset, LogEventLevel.Information, null, MessageTemplate.Empty, Array.Empty<LogEventProperty>());
+        var evt = new LogEvent(logTimestampWithTimeZoneOffset, LogEventLevel.Debug, null,
+            new MessageTemplate(Enumerable.Empty<MessageTemplateToken>()), Enumerable.Empty<LogEventProperty>());
         var sw = new StringWriter();
         formatter.Format(evt, sw);
         // expect time in local time, unchanged from the input, the +02:00 offset should not affect the output
@@ -418,7 +419,8 @@ public class OutputTemplateRendererTests
     {
         var logTimestampWithTimeZoneOffset = DateTimeOffset.Parse("2024-09-03T14:15:16.079+02:00", CultureInfo.InvariantCulture);
         var formatter = new OutputTemplateRenderer(ConsoleTheme.None, "{UtcTimestamp:yyyy-MM-dd HH:mm:ss}", CultureInfo.InvariantCulture);
-        var evt = new LogEvent(logTimestampWithTimeZoneOffset, LogEventLevel.Information, null, MessageTemplate.Empty, Array.Empty<LogEventProperty>());
+        var evt = new LogEvent(logTimestampWithTimeZoneOffset, LogEventLevel.Debug, null,
+            new MessageTemplate(Enumerable.Empty<MessageTemplateToken>()), Enumerable.Empty<LogEventProperty>());
         var sw = new StringWriter();
         formatter.Format(evt, sw);
         // expect time in UTC, the +02:00 offset must be applied to adjust the hour
