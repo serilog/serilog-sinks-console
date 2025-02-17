8b58251fca62380b9105d8c35e8418989bb2dfbf
Manuel Rinnhofer <manuel.rinnhofer@gmail.com>
2025-02-17 23:14:57 +0100

    Unit tests for rendering UtcTimestamp/Timestamp switched from Fact to Theory to improve maintainability
    For better maintainability the multi-line strings with several tokens and formats of Fact unit tests are replaced by Theory with InlineData for better maintainability/readability.
    
    Relates to #164

--- test/Serilog.Sinks.Console.Tests/Output/OutputTemplateRendererTests.cs
+++ test/Serilog.Sinks.Console.Tests/Output/OutputTemplateRendererTests.cs
@@ -401,49 +401,35 @@ public class OutputTemplateRendererTests
         Assert.Equal($"{traceId}/{spanId}", sw.ToString());
     }
 
-    [Fact]
-    public void TimestampTokenRendersLocalTime()
+    [Theory]
+    [InlineData("{Timestamp}", "09/03/2024 14:15:16 +02:00")] // Default Format
+    [InlineData("{Timestamp:o}", "2024-09-03T14:15:16.0790000+02:00")] // Round-trip Standard Format String
+    [InlineData("{Timestamp:yyyy-MM-dd HH:mm:ss}", "2024-09-03 14:15:16")] // Custom Format String
+    public void TimestampTokenRendersLocalTime(string actualToken, string expectedOutput)
     {
         var logTimestampWithTimeZoneOffset = DateTimeOffset.Parse("2024-09-03T14:15:16.079+02:00", CultureInfo.InvariantCulture);
-        var formatter = new OutputTemplateRenderer(ConsoleTheme.None,
-            """
-            Default Format: {Timestamp}
-            Round-trip Standard Format String: {Timestamp:o}
-            Custom Format String: {Timestamp:yyyy-MM-dd HH:mm:ss}
-            """,
-            CultureInfo.InvariantCulture);
+        var formatter = new OutputTemplateRenderer(ConsoleTheme.None, actualToken, CultureInfo.InvariantCulture);
         var evt = new LogEvent(logTimestampWithTimeZoneOffset, LogEventLevel.Debug, null,
             new MessageTemplate(Enumerable.Empty<MessageTemplateToken>()), Enumerable.Empty<LogEventProperty>());
         var sw = new StringWriter();
         formatter.Format(evt, sw);
         // expect time in local time, unchanged from the input, the +02:00 offset should not affect the output
-        Assert.Equal("""
-                     Default Format: 09/03/2024 14:15:16 +02:00
-                     Round-trip Standard Format String: 2024-09-03T14:15:16.0790000+02:00
-                     Custom Format String: 2024-09-03 14:15:16
-                     """, sw.ToString());
+        Assert.Equal(expectedOutput, sw.ToString());
     }
 
-    [Fact]
-    public void UtcTimestampTokenRendersUtcTime()
+    [Theory]
+    [InlineData("{UtcTimestamp}", "09/03/2024 12:15:16")] // Default Format
+    [InlineData("{UtcTimestamp:o}", "2024-09-03T12:15:16.0790000Z")] // Round-trip Standard Format String
+    [InlineData("{UtcTimestamp:yyyy-MM-dd HH:mm:ss}", "2024-09-03 12:15:16")] // Custom Format String
+    public void UtcTimestampTokenRendersUtcTime(string actualToken, string expectedOutput)
     {
         var logTimestampWithTimeZoneOffset = DateTimeOffset.Parse("2024-09-03T14:15:16.079+02:00", CultureInfo.InvariantCulture);
-        var formatter = new OutputTemplateRenderer(ConsoleTheme.None,
-            """
-            Default Format: {UtcTimestamp}
-            Round-trip Standard Format String: {UtcTimestamp:o}
-            Custom Format String: {UtcTimestamp:yyyy-MM-dd HH:mm:ss}
-            """,
-            CultureInfo.InvariantCulture);
+        var formatter = new OutputTemplateRenderer(ConsoleTheme.None, actualToken, CultureInfo.InvariantCulture);
         var evt = new LogEvent(logTimestampWithTimeZoneOffset, LogEventLevel.Debug, null,
             new MessageTemplate(Enumerable.Empty<MessageTemplateToken>()), Enumerable.Empty<LogEventProperty>());
         var sw = new StringWriter();
         formatter.Format(evt, sw);
         // expect time in UTC, the +02:00 offset must be applied to adjust the hour
-        Assert.Equal("""
-                     Default Format: 09/03/2024 12:15:16
-                     Round-trip Standard Format String: 2024-09-03T12:15:16.0790000Z
-                     Custom Format String: 2024-09-03 12:15:16
-                     """, sw.ToString());
+        Assert.Equal(expectedOutput, sw.ToString());
     }
 }
